91f5a82e2dff48725ad8a77c68821c99be1d045a
erutkovskis <edvards.rutkovskis@gmail.com>
2020-12-31 20:24:38 +0000

    Managed to get PDF calculation going. Non-vectorised, with loop for now.

--- part1/task2/task.m
+++ part1/task2/task.m
@@ -1,6 +1,15 @@
 pd = makedist('Normal'); % normal distribution
-x = random(pd,[10000,2]);
-mu = mean(x);
-sigma = cov(x);
+x = random(pd,[2,10000]);
+sigma = cov(x.');
 detsigma = det(sigma);
-p = 1/(2*pi*sqrt(detsigma)).*exp((-0.5*(x-mu).').*sigma.^(-1).*(x-mu));
+mu = mean(x,2);
+%i = 1:size(x,2);
+p = [];
+for i = 1:size(x,2)
+    pcur = (1/(2*pi*sqrt(detsigma)))*exp(-0.5*((x(:,i)-mu).')*(sigma^(-1))*(x(:,i)-mu));
+    p = [p,pcur];
+end
+
+% [X1, X2] = meshgrid(x);
+% figure;
+% plot3(X1,X2,p);
