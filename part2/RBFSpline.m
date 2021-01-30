1d0b6e9f86b698c76ab60d451800f15049a6af61
erutkovskis <edvards.rutkovskis@gmail.com>
2021-01-30 15:36:42 +0000

    Arranged functions fit,evaluate and the kernel into the class template. Functions separately seem to be working. Need to understand how to correctly organise the class.

--- part2/RBFSpline.m
+++ part2/RBFSpline.m
@@ -22,23 +22,77 @@ classdef RBFSpline
             outputArg = obj.Property1 + inputArg;
         end
         
-        function coef = fit(source_pts, target_pts)
+        function alpha = fit(source_pts, target_pts, lambda, sigmas)
             %FIT Summary of this function goes here
             %   Detailed explanation goes here
 
-            P = source_pts;
-            col_num = size(source_pts,2);
-            Ms = size(target_pts,2);
-            q = target_pts;
-            r = norm(P-q);
-            K = exp(-r.^2);
 
-            matrix1 = [K, P; P.', zeros(col_num+1)];
-            matrix2 = [q; zeros(col_num+1,Ms)];
+            [src_rown,src_coln] = size(source_pts); % useful dimentions
+            [tgt_rown,tgt_coln] = size(target_pts);
 
-            coef = matrix1 \ matrix2;
+
+            r = zeros(src_rown); % pre-allocate
+
+            % norm and RBF of the source points
+            for ii = 1:src_rown
+                for jj = (ii+1):src_rown
+                    r(ii,jj)=norm(source_pts(ii,:)-source_pts(jj,:));
+                    r(jj,ii)=r(ii,jj); % size n x n
+                end
+            end
+
+            % P = ones(src_rown+1); % n+1 x n+1
+            K = exp(-r.^2 / 2.*sigmas.^2); % matrix of gaussians, nxn
+            W = diag(sigmas.^(-2)); % landmark localization errors, nxn
+            target_pts = padarray(target_pts,[size(K,1),(size(K,2)-size(target_pts,2))]);
+            alpha = (K + lambda * W.^(-1)) \ target_pts; % solve the equation from the paper wrt alpha, nxn 
+
         end
 
+        function transf_set = evaluate(query_pts,control_pts,alpha)
+            %EVALUATE Summary of this function goes here
+            %   Detailed explanation goes here
+
+
+            [qry_rown,qry_coln] = size(query_pts); % useful dimentions
+
+            r = zeros(qry_rown); % pre-allocate
+
+            % norm and RBF of the "x - p" from the paper, assuming control points are x
+            for ii = 1:qry_rown
+                for jj = 1:control_pts
+                    r(ii,jj)=norm(query_pts(ii,:)-control_pts(jj,:));
+                end
+            end
+
+            % P = ones(src_rown+1); % n+1 x n+1
+            K = exp(-r.^2); % nxn
+            transf_set = K * alpha; % transformed points
+
+        end
+        
+        function kernel = kernel_gaussian(query_pts,control_pts,lambda,sigmas)
+            %UNTITLED Summary of this function goes here
+            %   Detailed explanation goes here
+
+            [qry_rown,qry_coln] = size(query_pts); % useful dimentions
+
+            r = zeros(qry_rown); % pre-allocate
+
+            % norm and RBF of the "x - p" from the paper, assuming control points are x
+            for ii = 1:qry_rown
+                for jj = 1:control_pts
+                    r(ii,jj)=norm(query_pts(ii,:)-control_pts(jj,:));
+                end
+            end
+
+            % P = ones(src_rown+1); % n+1 x n+1
+
+            K = exp(-r.^2 / 2.*sigmas.^2); % matrix of gaussians, nxn
+            W = diag(sigmas.^(-2)); % landmark localization errors, nxn
+            kernel = K+lambda*W.^(-1);
+
+        end
 
     end
 end
