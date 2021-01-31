fa8ad5e8f2928e081a41510ad9c5f5406a299e56
erutkovskis <edvards.rutkovskis@gmail.com>
2021-01-31 15:50:20 +0000

    Image read/write functions now handle dimensions correctly. Added some comments. Finishing task 1.

--- part1/task1/simple_image_read.m
+++ part1/task1/simple_image_read.m
@@ -13,8 +13,8 @@ end
 % Implementation
 FileToRead = fopen(FilepathToRead,'r'); % open test file in r mode
 ImgSize = fread(FileToRead,[1,3]);
-intensity = fread(FileToRead,[224,224*30],'*int16'); % read intensity values into 2D colsx(rows*depth) matrix in int16
-intensity = reshape(intensity,[224,224,30]); % reshape 2D matrix into 3D
+intensity = fread(FileToRead,[ImgSize(1),ImgSize(2)*ImgSize(3)],'*int16'); % read intensity values into 2D colsx(rows*depth) matrix in int16
+intensity = reshape(intensity,[ImgSize(1),ImgSize(2),ImgSize(3)]); % reshape 2D matrix into 3D
 voxels = fread(FileToRead,[1,3],'*single');
 
 fclose(FileToRead);
--- part1/task1/task.m
+++ part1/task1/task.m
@@ -1,21 +1,17 @@
-%% Load example_image with vol and voxdims
-load('../../data/example_image.mat');
-%% Path to image.sim
-FilepathToWrite = '../../data/image.sim';
-%% Write image intensity values and voxel dimension into the image.sim
-simple_image_write(FilepathToWrite,vol,voxdims);
-%% 
-FilepathToRead = FilepathToWrite;
-%% Read image.sim and get intensity and voxel dimension values
-[intensity,voxdimens] = simple_image_read(FilepathToRead);
+%% Read/Write workflow
+load('../../data/example_image.mat'); % Load example_image with vol and voxdims
+FilepathToWrite = '../../data/image.sim'; % Path to image.sim
+simple_image_write(FilepathToWrite,vol,voxdims); % Write image intensity values and voxel dimension into the image.sim 
+FilepathToRead = FilepathToWrite; % path to the just written file
+[intensity,voxdimens] = simple_image_read(FilepathToRead); % Read image.sim and get intensity and voxel dimension values
 
 %% Attempt on scaling and visualization
-img_dims = size(intensity);
-x_axis = (0:img_dims(1)-1) * voxdims(1);
-y_axis = (0:img_dims(2)-1) * voxdims(2);
-z_axis = (0:img_dims(3)-1) * voxdims(3);
+img_dims = size(intensity); 
+x_axis = (0:img_dims(1)-1) * voxdims(1); % scale x axis according to voxel dimensions
+y_axis = (0:img_dims(2)-1) * voxdims(2); % scale y axis 
+z_axis = (0:img_dims(3)-1) * voxdims(3); % scale z axis 
 
-subplot(3,1,1);
+subplot(3,1,1); % visualise different slices
 imagesc(y_axis,z_axis,squeeze(intensity(200,:,:))'); axis image; colormap(gray); title('YZ plane,200th slice');
 subplot(3,1,2);
 imagesc(x_axis,z_axis,squeeze(intensity(:,150,:))'); axis image; colormap(gray); title('XZ plane,150th slice');
