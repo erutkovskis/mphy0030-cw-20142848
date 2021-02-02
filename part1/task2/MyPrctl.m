6cac46c8574b81b8518d81deed883be1c1dc577c
erutkovskis <edvards.rutkovskis@gmail.com>
2021-02-02 22:41:25 +0000

    Added function descriptions. Added visualisation saving.

--- part1/task2/MyPrctl.m
+++ part1/task2/MyPrctl.m
@@ -1,9 +1,12 @@
 function pct = MyPrctl(PDF,percentile)
-% pct = prcntl(data,percentile)
-% Calculates percentile using nearest rank method
+% pct = MyPrctl(PDF,percentile)
+% 
+% Calculates percentile using nearest rank method.
 % INPUTS:
-% data          array of values
-% percentile    percentile to be calculated
+%   PDF             PDF values
+%   percentile      percentile to be calculated
+% Outputs:
+%   pct             PDF values corresponding to the percentile
 
 xSorted = sort(PDF,2); % sort the distribution row-wise (PDF considered to be 1xn)
 N = numel(xSorted(1,:)); % number of elements in the PDF
--- part1/task2/task.m
+++ part1/task2/task.m
@@ -1,25 +1,7 @@
-% PDF calculation
-pd = makedist('Normal'); % generate normal distribution
-x = random(pd,[10000,3]); % create a set of 10000 x1,x2 and x3 variables, dimensionality of x1 and x2 is [2x1]
-sigma = cov(x); % covariance matrix, works on 10000x3 matrix
-detsigma = det(sigma); % determinant of covariance matrix
-mu = mean(x); % mean vector, operates on the variables 3x10000
-xtr = x.'; % transpose x for the PDF function
-mutr = mu.'; % transpose mean calculation accordingly
-% The following loop calcualtes PDF for each x1,x2 and x3 according to
-% multivatiate Gaussian function
-p = [];
-for i = 1:size(xtr,2)
-    pcur = (1/(2*(pi^1.5)*sqrt(detsigma)))*exp(-0.5*(((xtr(:,i))-mutr).')*(sigma^(-1))*(xtr(:,i)-mutr));
-    p = [p,pcur];
-end
-
-% figure;scatter3(x(1,:),x(2,:),p,'.') % scatter3 visualization
-
 %% Generate  samples, calculate Gaussian PDF
 x = randn(10000,3); % create a set of 10000 x1,x2 and x3 variables
 sigma = cov(x); % covariance matrix, works on 10000x3 matrix
-mu = mean(x); % mean vector, operates on the variables 3x10000
+mu = mean(x); % mean vector
 p = gaussian_pdf(x,mu,sigma);   
 
 %% Percentiles calculation
@@ -45,15 +27,13 @@ x90 = x(i90,:);
 DT10 = delaunay(x10(:,1),x10(:,2),x10(:,3)); % create Delanay triangulation matrix from X10 points (3D crossection for the 10th percentile)
 DT50 = delaunay(x50(:,1),x50(:,2),x50(:,3)); % create Delanay triangulation matrix from X50 points (3D crossection for the 50th percentile)
 DT90 = delaunay(x90(:,1),x90(:,2),x90(:,3)); % create Delanay triangulation matrix from X10 points (3D crossection for the 90th percentile)
-% figure; trisurf(DT10,x10(:,1),x10(:,2),x10(:,3)); % Visualize using trisurf in separate windows
-% figure; trisurf(DT50,x50(:,1),x50(:,2),x50(:,3)); 
-% figure; trisurf(DT90,x90(:,1),x90(:,2),x90(:,3)); 
-% figure; trisurf(DT10,x10(:,1),x10(:,2),x10(:,3), 'FaceAlpha',0.9); hold on;
-% trisurf(DT50,x50(:,1),x50(:,2),x50(:,3),'FaceAlpha',0.9); hold on;
-% trisurf(DT90,x90(:,1),x90(:,2),x90(:,3),'FaceAlpha',0.9); hold off;
 subplot(1,3,1);
 trisurf(DT10,x10(:,1),x10(:,2),x10(:,3)); title('10th percentile PDF ellipsoid');
 subplot(1,3,2);
 trisurf(DT50,x50(:,1),x50(:,2),x50(:,3)); title('50th percentile PDF ellipsoid');
 subplot(1,3,3);
 trisurf(DT90,x90(:,1),x90(:,2),x90(:,3)); title('90th percentile PDF ellipsoid');
+
+%% Save visualizations 
+Image = getframe(gcf);
+imwrite(Image.cdata,'ellipsoids.jpg');
