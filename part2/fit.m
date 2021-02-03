7e90a8d4908cbe2248d3cdb4b4f5afb2c13354f2
erutkovskis <edvards.rutkovskis@gmail.com>
2021-02-03 22:19:32 +0000

    Did as much as I could

--- part2/fit.m
+++ part2/fit.m
@@ -1,4 +1,4 @@
-function alpha = fit(source_pts, target_pts)
+function [alpha,alpha2] = fit(source_pts, target_pts, lambda, sigmas)
 %FIT Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -13,30 +13,32 @@ r = zeros(src_rown); % pre-allocate
 for ii = 1:src_rown
     for jj = (ii+1):src_rown
         r(ii,jj)=norm(source_pts(ii,:)-source_pts(jj,:));
-        r(jj,ii)=r(ii,jj); % size n x n
+        r(jj,ii)=r(ii,jj); % size n x n, calculates the opposite side of the matrix
     end
 end
 
 % P = ones(src_rown+1); % n+1 x n+1
-K = exp(-r.^2); % nxn
-
-% Solve for each dimension?
-alpha = zeros(tgt_rown);
-for x = 1:tgt_rown
-    alpha(x,1) = K(x,:) / target_pts(x,1);
-end
-for y = 1:tgt_rown
-    alpha(y,2) = K(y,:) / target_pts(y,2);
-end
-for z = 1:tgt_rown
-    alpha(z,3) = K(z,:) / target_pts(z,3);
-end
-
-
+K = exp(-r.^2 / 2.*sigmas.^2); % matrix of gaussians, nxn
+W = diag(sigmas.^(-2)); % landmark localization errors, nxn
+% target_pts = padarray(target_pts,[size(K,1),(size(K,2)-size(target_pts,2))]);
+alpha = (K + lambda * W.^(-1)) \ target_pts; % solve the equation from the paper wrt alpha, nxn 
+alpha2 = K \ target_pts; % solution with no lambda, but at least it works
 end
 
 % matrix1 = [K, P; P.', zeros()]; % lefthandside matrix
 % matrix2 = [target_pts; zeros()]; % righthadside matrix
 
 % coef = matrix1 \ matrix2; % solve wrt alpha
-% alpha = K \ qk; 
+
+
+% % Solve for each dimension?
+% alpha = zeros(tgt_rown);
+% for x = 1:tgt_rown
+%     alpha(x,1) = K(x,:) / target_pts(x,1);
+% end
+% for y = 1:tgt_rown
+%     alpha(y,2) = K(y,:) / target_pts(y,2);
+% end
+% for z = 1:tgt_rown
+%     alpha(z,3) = K(z,:) / target_pts(z,3);
+% end
